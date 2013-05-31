844e98f3f9beca26d4bf659c767f1fac1ea416d2
Radu Dumitriu <rdumitriu@gmail.com>
2013-05-31 03:46:47 +0300

    base url fix

--- src/main/java/ro/agrade/jira/rewards/utils/JIRAUtils.java
+++ src/main/java/ro/agrade/jira/rewards/utils/JIRAUtils.java
@@ -27,8 +27,8 @@ public final class JIRAUtils {
     public static String getRelativeJIRAPath(ApplicationProperties props) {
         URI uri = URI.create(props.getString("jira.baseurl"));
         String rawBase = uri.getPath(); //this returns the last path of the JIRA install
-        return ((rawBase.length() > 0 && rawBase.charAt(rawBase.length() - 1) == '/')
-                            ? rawBase.substring(0, rawBase.length() - 1)
+        return ((rawBase.length() > 0)
+                            ? (rawBase.charAt(rawBase.length() - 1) == '/' ? rawBase.substring(0, rawBase.length() - 1) : rawBase)
                             : "");
     }
 }
